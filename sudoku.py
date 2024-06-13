e28f07b0ad56f3cba8c722f0956edc35ceb87291
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-06-13 13:15:41 +0100

    feat: find_next_empty function - Implemented find_next_empty function to check if a particular position in the puzzle is empty. - returns the position if it is empty and None if occupied
    feat: solve_sudoku function returns True:
    - Implemented code which returns true if there are no empty positions in the game

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,30 @@
 def find_next_empty(puzzle):
     # find the next row, col on the puzzle that's not filled yet
     # in the puzzle, representing any open spaces with -1
-    # return row, col tuple(or None, None) if there in none)
+    # return row, col tuple(or (None, None) if there is no empty space)
 
     # keep in mind that we are using 0-8 for our indices
-    pass
+    for row in range(9):
+        for col in range(9):
+            if puzzle[row][col] == -1:
+                return row, col
+    # if there are no empty spaces left, i.e -1
+    return None, None
+
+
 def solve_sudoku(puzzle):
     # solve sudoku using backtracking technique
     # our puzzle is a list of lists, where each inner list is a row in our sudoku puzzle
     # return whether a solution exists
     # mutates puzzle to be the solution (if the solution exists)
 
-
     # step 1: choose somewhere on the puzzle to make a guess
     row, col = find_next_empty(puzzle)
+
+    # step 1.1: if there's nowhere left, them we're done because we only allowed valid inputs
+    if row is None:
+        return True
+    # step 2: if there is a place to put a number, then make a gues between 1 and 9
+    for guess in range(1, 10):
+        # step 3: checking ig this is a valid guess
+        pass
