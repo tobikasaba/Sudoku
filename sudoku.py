92512e979c1f7dbe4262f3ee9df4d9acfc280eeb
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-06-13 18:20:44 +0100

    feat: implemented backtracking algorithm to solve Sudoku puzzles - implemented the solve_sudoku function that uses backtracking to solve the puzzle. - included validation checks to ensure guesses are valid before placing them. - added recursion to explore possible solutions. - implemented backtracking to reset guesses when necessary. - ensured the function mutates the puzzle in-place to reflect the solution. By adding current guess to the puzzle
    style: changed format of code to make it cleaner and readable
    
    fix: ensure the 3x3 square check in the is_valid function returns false if puzzle[row][col] == guess

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 def find_next_empty(puzzle):
     # find the next row, col on the puzzle that's not filled yet
     # in the puzzle, representing any open spaces with -1
@@ -8,7 +11,7 @@ def find_next_empty(puzzle):
         for col in range(9):
             if puzzle[row][col] == -1:
                 return row, col
-    # if there are no empty spaces left, i.e -1
+    # if there are no empty spaces left, i.e. -1
     return None, None
 
 
@@ -34,17 +37,18 @@ def is_valid(puzzle, guess, row, col):
     # first get where 3x3 square starts.
     # split the entire board into 3 chunks of 3x3 squares.
     # iterate over the 3 values in the row/column.
-    row_start = (row//3) *3
-    col_start = (col//3)*3
+    row_start = (row // 3) * 3
+    col_start = (col // 3) * 3
 
     # iterate over the 3x3 square selected based on the predetermined starting position
-    for row in range(row_start, row_start+3):
-        for col in range(col_start, col_start+3):
-            if puzzle[row][col]:
+    for row in range(row_start, row_start + 3):
+        for col in range(col_start, col_start + 3):
+            if puzzle[row][col] == guess:
                 return False
 
     # if we get here, the checks pass
     return True
+
 
 def solve_sudoku(puzzle):
     # solve sudoku using backtracking technique
@@ -54,12 +58,41 @@ def solve_sudoku(puzzle):
 
     # step 1: choose somewhere on the puzzle to make a guess
     row, col = find_next_empty(puzzle)
-
     # step 1.1: if there's nowhere left, them we're done because we only allowed valid inputs
     if row is None:
         return True
+
     # step 2: if there is a place to put a number, then make a gues between 1 and 9
     for guess in range(1, 10):
-        # step 3: checking ig this is a valid guess
+        # step 3: checking if this is a valid guess
         if is_valid(puzzle, guess, row, col):
-            pass
+            # step 3.1 if this is valid, then place that guess on the puzzle
+            puzzle[row][col] = guess
+            # now recurse using this puzzle.
+            # step 4: recursively call the function
+            if solve_sudoku(puzzle):
+                return True
+        # step 5: if not valid OR if our guess does not solve the puzzle,
+        # then we need to backtrack and try a new number
+        puzzle[row][col] = -1
+
+    # step 6: if none of the numbers that we try work, then this puzzle in UNSOLVABLE!!
+    return False
+
+
+if __name__ == "__main__":
+    example_board = [
+        [3, 9, -1, -1, 5, -1, -1, -1, -1],
+        [-1, -1, -1, 2, -1, -1, -1, -1, 5],
+        [-1, -1, -1, 7, 1, 9, -1, 8, -1],
+
+        [-1, 5, -1, -1, 6, 8, -1, -1, -1],
+        [2, -1, 6, -1, -1, 3, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1, 4],
+
+        [5, -1, -1, -1, -1, -1, -1, -1, -1],
+        [6, 7, -1, 1, -1, 5, -1, 4, -1],
+        [1, -1, 9, -1, -1, -1, 2, -1, -1]
+    ]
+    print(solve_sudoku(example_board))
+    pprint(example_board)
